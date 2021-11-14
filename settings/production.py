5d44d1a28fba1a90288c352844f653183dfe0160
Sergey <serg78kh@yandex.ru>
2021-11-15 00:11:54 +0300

    v1: Hello World! | Add files for server

---
+++ settings/production.py
@@ -0,0 +1,20 @@
+# TG_TOKEN = "1363841624:AAEbh2XaTpf0wVK6nJaDTY6mxNs4gtpjTBY"
+# TG_API_URL = None
+import logging.config
+import os
+
+import sentry_sdk
+
+
+# Путь до коренной папки `tele_bot`
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+
+TG_TOKEN = "2073227161:AAEQcHMYsawinAZpM_0tJlAvyQ8Ggj-oJdc"
+
+# На сервере не используем прокси-URL
+TG_API_URL = None
+
+# Sentry
+sentry_sdk.init(
+    dsn="1363841624:AAEbh2XaTpf0wVK6nJaDTY6mxNs4gtpjTBY",
+)
